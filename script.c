d58ce62a7c27e727b4afedc04ff0a671400b6e23
nouhaila_khaouti <96340163+Nounixa@users.noreply.github.com>
2022-09-15 09:46:43 +0100

    etape 1
    race structure

---
+++ script.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// Structures section
+struct Race
+{
+    int numberOfLaps;
+    int currentLap;
+    char firstPlaceDriverName[20];
+    char firstPlaceRaceCar[20];
+};
+
+// Print functions section
+
+// Logic functions section
+
+int main()
+{
+    srand(time(0));
+};
